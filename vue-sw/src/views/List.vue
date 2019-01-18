515f01c47b6f32420ef6da249e73af311a24a24a
Nicky Chen <nicky.chen0@gmail.com>
2019-01-17 22:00:26 -0500

    axios call

---
+++ vue-sw/src/views/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>List</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      films: []
+    };
+  },
+  created: function() {
+    axios.get("https://swapi.co/api/films").then(res => {
+      /*
+			there is no "id" field, just a URL one - so let's set it manually
+			*/
+
+      this.films = res.data.results.map(film => {
+        let parts = film.url.split("/");
+        film.id = parts[parts.length - 2];
+        return film;
+      });
+    });
+  }
+};
+</script>
+
+<style>
+</style>
